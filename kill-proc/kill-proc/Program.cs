e402b10118ed58e2595485bb67aa4a5206618f44
Glenn Ferrie <glenn.ferrie@gmail.com>
2013-08-30 22:44:59 -0400

    finished (1 dimensional)

--- kill-proc/kill-proc/Program.cs
+++ kill-proc/kill-proc/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -13,16 +14,27 @@ namespace killproc
             if (args == null || args.Length == 0)
                 throw new ArgumentNullException("No arguments. 'processId' expected.");
 
-            try
+            #region 1 Arg
+            if (args.Length == 1)
             {
+                try
+                {
+                    int id;
+                    if (!int.TryParse(args[0], out id))
+                        throw new ArgumentNullException("First Arg is not an Integer, pass a active processid (ex. 1809) ");
 
+                    var proc = Process.GetProcessById(id);
+                    proc.Kill();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: {0}", ex);
+                    Console.ReadLine();
+                }
+
+                return;
             }
-            catch (Exception ex)
-            {
-
-            }
-
-            if (args.Length > 1 && args[1]
+            #endregion
         }
     }
 }
