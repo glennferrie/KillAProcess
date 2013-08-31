ae44dfd642c5e088879e15a09003912cbe1fd907
Glenn Ferrie <glenn.ferrie@gmail.com>
2013-08-30 22:30:41 -0400

    first checkin

---
+++ kill-proc/kill-proc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace killproc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+                throw new ArgumentNullException("No arguments. 'processId' expected.");
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            if (args.Length > 1 && args[1]
+        }
+    }
+}
